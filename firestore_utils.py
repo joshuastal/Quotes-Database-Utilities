2b1a948690732626cac96178beb8f017951d6084
Joshua S. <johnsilverknight@gmail.com>
2026-07-23 00:25:39 -0500

    Added persistent storage to firestore_utils.py for returned list

--- firestore_utils.py
+++ firestore_utils.py
@@ -1,4 +1,7 @@
+import pickle
+
 from google.cloud import firestore
+from google.cloud.firestore_v1 import DocumentReference
 from google.oauth2 import service_account
 
 
@@ -15,12 +18,12 @@ class Quote:
 
 class FirestoreClient:
     def __init__(self):
-        self._db = firestore.Client(
+        self.client = firestore.Client(
             project=service_account.Credentials.from_service_account_file(  # pyright: ignore
-                "Quotes_App_Utilities/test-6aa80-firebase-adminsdk-troo8-435926642e.json"
+                "test-6aa80-firebase-adminsdk-troo8-435926642e.json"
             ).project_id,
             credentials=service_account.Credentials.from_service_account_file(  # pyright: ignore
-                "Quotes_App_Utilities/test-6aa80-firebase-adminsdk-troo8-435926642e.json"
+                "test-6aa80-firebase-adminsdk-troo8-435926642e.json"
             ),
         )
 
@@ -29,7 +32,7 @@ class FirestoreClient:
 
     def get_collection(self) -> tuple[list[Quote], list[str]]:
 
-        for document in self._db.collection("Quotes").stream():
+        for document in self.client.collection("Quotes").stream():
             data = document.to_dict()
 
             # Quote(**data) takes a dictionary and passes the key-value pairs to the Quote constructor as named arguments
@@ -37,7 +40,7 @@ class FirestoreClient:
             # Quote: value
             # tags: value
             self._documents.append(Quote(**data))  # pyright: ignore
-            self._document_references.append(document.reference)  # pyright: ignore
+            self._document_references.append(document.reference.path)  # pyright: ignore
 
             if data is None:
                 raise ValueError("Document data is None")
@@ -51,10 +54,11 @@ class FirestoreClient:
         return self._documents, self._document_references
 
     def summarize_collection(
-        self, collection: list[Quote], collection_references: list[str]
+            self, collection: list[Quote], collection_references: list[DocumentReference]
     ):
         for quote in collection:
-            print(f"""
+            print(
+                f"""
             Author: {quote.author}
             Quote: {quote.quote}
             Tags: {quote.tags}
@@ -67,7 +71,16 @@ class FirestoreClient:
 
 def main():
     db = FirestoreClient()
-    collection, collection_references = db.get_collection()
+
+    quotes_file = "quotes.pk"
+
+    with open(quotes_file, 'wb') as f:
+        pickle.dump(db.get_collection(), f)
+
+    with open(quotes_file, 'rb') as f:
+        collection, references = pickle.load(f)
+
+    collection_references = [db.client.document(path) for path in references]
     db.summarize_collection(collection, collection_references)
 
 
